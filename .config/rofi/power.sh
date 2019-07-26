9e641654e7faa0c94b87a0c6ddc21a51e80412b1
acxz <17132214+acxz@users.noreply.github.com>
2019-07-26 11:45:03 -0400

    Fixed i3lock commands

--- .config/rofi/power.sh
+++ .config/rofi/power.sh
@@ -7,7 +7,7 @@
 
 OPTIONS="lock\nsuspend\nlogout\nreboot\npoweroff"
 
-LAUNCHER="rofi -lines 5 -location 1 -xoffset 50 -yoffset 100 -width 10 -dmenu -i -p power:"
+LAUNCHER="rofi -lines 5 -location 1 -xoffset 50 -yoffset 100 -width 10 -dmenu -i -p power"
 
 option=`echo -e $OPTIONS | $LAUNCHER`
 if [ ${#option} -gt 0 ]
@@ -20,13 +20,13 @@ then
         systemctl poweroff
         ;;
       lock)
-        i3lock -k --datestr="%a %m/%d/%y" -i ~/Pictures/night_sky_blur.png --insidevercolor=1e2832ff --insidewrongcolor=d73737ff --insidecolor=1e2832ff --ringvercolor=1e2832ff --ringwrongcolor=1e2832ff --ringcolor=1e2832ff --linecolor=1e2832ff --separatorcolor=1e2832ff --textcolor=afffa3ff --keyhlcolor=60ac39ff --bshlcolor=d73737ff
+        i3lock -k --datestr="%a %m/%d/%y" -i ~/Pictures/night_sky_blur.png --insidevercolor=1e2832ff --insidewrongcolor=d73737ff --insidecolor=1e2832ff --ringvercolor=1e2832ff --ringwrongcolor=1e2832ff --ringcolor=1e2832ff --linecolor=1e2832ff --separatorcolor=1e2832ff --keyhlcolor=60ac39ff --bshlcolor=d73737ff
         ;;
       logout)
         i3-msg exit
         ;;
       suspend)
-        i3lock -k --datestr="%a %m/%d/%y" -i ~/Pictures/night_sky_blur.png --insidevercolor=1e2832ff --insidewrongcolor=d73737ff --insidecolor=1e2832ff --ringvercolor=1e2832ff --ringwrongcolor=1e2832ff --ringcolor=1e2832ff --linecolor=1e2832ff --separatorcolor=1e2832ff --textcolor=dd3cffff --keyhlcolor=60ac39ff --bshlcolor=d73737ff;
+        i3lock -k --datestr="%a %m/%d/%y" -i ~/Pictures/night_sky_blur.png --insidevercolor=1e2832ff --insidewrongcolor=d73737ff --insidecolor=1e2832ff --ringvercolor=1e2832ff --ringwrongcolor=1e2832ff --ringcolor=1e2832ff --linecolor=1e2832ff --separatorcolor=1e2832ff --keyhlcolor=60ac39ff --bshlcolor=d73737ff;
         systemctl suspend
         ;;
     esac
